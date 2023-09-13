6d1dd27629964798c8419ea5bdc3903a65b1e5f6
NF09830453 <fyre.lord31@gmail.com>
2023-09-13 17:18:01 -0400

    added a form

--- Form3.cs
+++ Form3.cs
@@ -10,24 +10,20 @@ using System.Windows.Forms;
 
 namespace Lab5
 {
-    public partial class Form2 : Form
+    public partial class Form3 : Form
     {
-        public Form2(double current, double calculatedResult)
+        public Form3(double current)
         {
             InitializeComponent();
             label1.Text = "Current Value Before: " + current.ToString();
-            if (calculatedResult < 0)
-            {
-                label2.Text = "Result: Invalid N/A";
-            }
-            else
-            {
-                label2.Text = "Result: " + calculatedResult.ToString();
-            }
-
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
